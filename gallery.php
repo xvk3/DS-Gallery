6be261e2511caf8150a841c23470a3c430fc654f
W <w@xvk3.net>
2021-07-02 07:48:28 +0100

    Initial commit

---
+++ gallery.php
@@ -0,0 +1,119 @@
+<html>
+	<head>
+		<title>Gallery</title>
+    <style>
+
+    *{
+      margin: 0 auto;
+    }
+
+    a {
+      text-decoration: none;
+    }
+
+    html {
+      background: #f3f3f3;
+    }
+    
+    #cont {
+      position: relative;
+      max-width:1920px;
+      overflow: hidden;
+    }
+
+    #cont a {
+      display: flex;
+      align-items: center;
+      justify-content: center;
+      position: absolute;
+      top:0;
+      width: 70px;
+      height: 100%;
+  
+      font-size: 2em;
+      font-weight: bold;
+      background: #fff;
+      color: #000;
+      opacity: .2;
+  
+      transition: .5s ease;
+    }
+
+    #cont a:hover {
+      opacity: 0.8;
+      width: 90px;
+      transition: .75s ease;
+    }
+
+    #cont img {
+      object-fit: fill;
+      width: 100%;
+    }
+
+    #left { left:0;}
+    #right{ right:0;}
+
+    </style>
+		<script type="application/javascript">
+
+		  <?php
+
+      	$dir = './DS-Screenshots';
+ 				$files = scandir($dir);
+
+        if(isset($_GET['index']))  {
+          echo "var index = " . $_GET['index'] . ";\n";
+        } else {
+          echo "var index = " . rand(1, sizeof($files)) . ";\n";
+        }
+
+ 				echo "var images = [\n";
+ 				for($x = 2; $x <= sizeof($files)-2; $x++)	{
+ 			  	echo "\"" . $files[$x] . "\",\n";
+ 			  }
+        echo "        \"" . $files[sizeof($files)-1] . "\"\n";
+        echo "];\n";
+
+ 			?>
+
+      function load()  {
+        document.getElementById("image").src = "DS-Screenshots/" + window.images[window.index];
+        document.getElementById("pindex").innerHTML = window.index;
+        window.history.replaceState(null, null, "?index=" + window.index);
+      }
+
+      function next() {
+        if(window.index > window.images.length-1) {
+          window.index = 0;
+        } else {
+          window.index = window.index + 1;
+        }
+        load();
+      }
+
+      function prev() {
+        if(window.index == 0)  {
+          window.index = window.images.length-1;
+        } else {
+          window.index = window.index - 1;
+        }
+        load();
+      }
+
+		</script>
+
+
+	</head>
+
+	<body onload="load()">
+
+    <div id="cont">
+      <img id="image" src="image.png"/>
+      <a id="left"  onclick="prev()">&lt;</a>
+      <a id="right" onclick="next()">&gt;</a>
+    </div>
+    <p id="pindex">0</p>
+ 
+	</body>
+
+</html>
